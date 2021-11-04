27a86205f87ea22d5313500bab2cb45144b2a557
wd <m15801428512@163.com>
2021-11-04 16:19:40 +0800

    修改路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -135,7 +135,7 @@ export const constantRoutes = [
 ]
 
 export default new Router({
-  mode: 'history', // 去掉url中的#
+  mode: 'hash', // 去掉url中的#
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
