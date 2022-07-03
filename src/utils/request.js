d8226be5a680564a637a49f1c268d790a4b7e4c8
ChatillonBourrasque <lmw2357520@qq.com>
2022-07-03 15:27:58 +0800

    huike-crm-web
    huike-crm-web

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,7 +10,8 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: process.env.VUE_APP_BASE_API,
+  //baseURL: process.env.VUE_APP_BASE_API,
+  baseURL: "http://127.0.0.1:8080",
   // 超时
   timeout: 10000
 })
--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://172.17.2.65:8080`,
+        target: `http://127.0.0.1:8080`,
         // target: `http://localhost:8080`,
         changeOrigin: true,
         pathRewrite: {
